3772a4b0b7fe0039bf43e61d3683b5d741e6df0d
andredri390-ctrl <andredri390@gmail.com>
2026-04-05 10:45:19 +0300

    Add files via upload

---
+++ dz5.py
@@ -0,0 +1,17 @@
+import colorama
+from colorama import Fore, Back, Style
+
+
+
+print(Fore.CYAN + "--- Інтроспекція бібліотеки Colorama ---")
+
+
+print(Fore.RED + "Цей текст червоний")
+print(Back.GREEN + Fore.BLACK + "Цей текст має зелений фон")
+print(Style.BRIGHT + "Це дуже яскравий текст")
+
+
+
+
+
+
